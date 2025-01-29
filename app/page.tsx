c0c5d8a66c0543b24078e31425d47f5cb68e96f1
Wenxiao <buwenxiao@outlook.com>
2025-01-28 19:23:08 -0500

    modify font for header

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
           priority
         />
         
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-ubuntu-mono)]">
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-ubuntu-sans)]">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
